806d3d9311e59f960453a7b45adefedce329584d
Sndolgov <dolgov.sergey84@gmail.com>
2017-12-19 21:51:01 +0300

    HW6 2 ADDED ROLE & FIXED Jdbc Tests

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
@@ -4,22 +4,49 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
 import javax.sql.DataSource;
-import java.util.List;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
 @Repository
-@Transactional (readOnly = true)
+@Transactional(readOnly = true)
 public class JdbcUserRepositoryImpl implements UserRepository {
 
     private static final BeanPropertyRowMapper<User> ROW_MAPPER = BeanPropertyRowMapper.newInstance(User.class);
+
+    private static Map<Integer, User> map = new HashMap<>();
+
+    private static final RowMapper MAPPER = new RowMapper() {
+        @Nullable
+        @Override
+        public Object mapRow(ResultSet resultSet, int i) throws SQLException {
+            Role role = Enum.valueOf(Role.class, resultSet.getString("role"));
+            User user = new User(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getString("email"),
+                    resultSet.getString("password"), resultSet.getInt("calories_per_day"),
+                    resultSet.getBoolean("enabled"), resultSet.getDate("registered"), Arrays.asList(role));
+
+            User fromMap = map.get(user.getId());
+
+            if (fromMap == null)
+                map.put(user.getId(), user);
+            else {
+                return fromMap.getRoles().add(role);
+            }
+            return user;
+        }
+    };
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -61,19 +88,24 @@ public class JdbcUserRepositoryImpl implements UserRepository {
 
     @Override
     public User get(int id) {
-        List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE id=?", ROW_MAPPER, id);
-        return DataAccessUtils.singleResult(users);
+
+        map.clear();
+        jdbcTemplate.query("SELECT * FROM users LEFT JOIN user_roles ON users.id = user_roles.user_id WHERE users.id=? ", MAPPER, id);
+        return DataAccessUtils.singleResult(map.values());
     }
 
     @Override
     public User getByEmail(String email) {
 //        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
-        List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
-        return DataAccessUtils.singleResult(users);
+        map.clear();
+        jdbcTemplate.query("SELECT * FROM users LEFT JOIN user_roles ON users.id = user_roles.user_id WHERE email=?", MAPPER, email);
+        return DataAccessUtils.singleResult(map.values());
     }
 
     @Override
     public List<User> getAll() {
-        return jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+        map.clear();
+        jdbcTemplate.query("SELECT * FROM users LEFT JOIN user_roles ON users.id = user_roles.user_id ORDER BY name, email", MAPPER);
+        return new ArrayList<>(map.values());
     }
 }
